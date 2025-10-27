dfc1963de9fc2e18729aba75759b87b768f20311
planazoo <unplanazoo@gmail.com>
2025-10-27 19:01:59 +0100

    chore: Eliminar log innecesario en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'app/app.dart';
 import 'features/calendar/domain/services/timezone_service.dart';
-import 'shared/services/logger_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +15,6 @@ void main() async {
   
   // Inicializar base de datos de timezones
   TimezoneService.initialize();
-  
-  // Log de inicio
-  LoggerService.debug('Aplicación iniciada', context: 'main');
   
   runApp(
     const ProviderScope(
